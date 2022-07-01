c95b7835fa29b17210a2a76a728d9d9476c62a7d
yycinm <yycinm@hanmail.net>
2022-07-01 18:19:38 +0900

    vvvv

--- 00javascript/20220624_if/js/scripttest.js
+++ 00javascript/20220624_if/js/scripttest.js
@@ -1,107 +1,62 @@
 var userInput = window.prompt("가위,바위,보를 입력하세요")
 console.log(userInput)
 if(userInput != "가위" && userInput != "바위" && userInput != "보"){
-    alert("가위,바위,보를 입력하시오")
-}
-
-var num = Math.floor(Math.random()*10);
-console.log(num)
-
-var comInput=""
-if(num < 3){
-    comInput ="가위";
-}else if(num < 6 ){
-    comInput = "바위";
+    alert("가위,바위,보 중 하나를 입력하시오")
 }else{
-    comInput = "보";
-}
-console.log(comInput)
+    var num = Math.floor(Math.random()*10);
+    console.log(num)
+    var comInput ="";
+    var result = ""
 
-var useInput=""
-if(useInput = "가위"){
-    if(comInput = "가위"){
-        if(userInput == comInput){
-        alert("비겼네요.다음기회에")
+    if(num<3){
+        comInput= "가위";
+    }else if(num <6 ){
+        comInput = "바위";
+    }else{
+        comInput = "보";
     }
-    if(comInput == "보"){
-        alert("당신이 이겼네요. ^^*")
+
+    if( useInput == "가위"){
+        if(comInput == "가위"){
+            result = "비김"
+        }
+        else if(comInput == "바위"){
+            result = "컴이 이겼습니다"
+        }
+        else{
+            result = "당신이 이겼습니다"
+        }
+
+
     }
-    if(comInput == "바위"){
-        alert("컴퓨터가 이겼습니다 ^^*")}}}
 
-else if(useInput = "바위"){
-    if(comInput = "바위"){
-        if(userInput == comInput){
-        alert("비겼네요.다음기회에")
+    if( userInput == "바위"){
+        if(comInput == "바위"){
+            result = "비김"
+        }
+        if(comInput == "보"){
+            result ="컴이 이겼습니다"
+        }
+        if(comInput == "가위"){
+            result ="당신이 이겼습니다"
+        }
     }
-    if(comInput == "가위"){
-        alert("당신이 이겼네요. ^^*")
-    }
-    if(comInputt == "보"){
-        alert("컴퓨터가 이겼습니다 ^^*")}}}
-
-else if(useInput = "바위"){
-    if(comInput = "바위"){
-        if(userInput == comInput){
-        alert("비겼네요.다음기회에")
-    }
-    if(comInput == "가위"){
-        alert("당신이 이겼네요. ^^*")
-    }
-    if(comInput == "보"){
-        alert("컴퓨터가 이겼습니다 ^^*")}}}
-
-        console.log(userInput)
-
-    
-// else if(num < 6 ){
-//     comInput = "바위";
-// }else{
-//     comInput = "보";
-// }
-// console.log(comInput)
-
-
-// if(comInput = "가위"){
-//     if(userInput == comInput){
-//     alert("비겼네요.다음기회에")
-// }
-// if(userInput == "바위"){
-//     alert("당신이 이겼네요. ^^*")
-// }
-// if(userInput == "보"){
-//     alert("컴퓨터가 이겼습니다 ^^*")}
-
-
-// }else if(comInput = "바위"){
-//         if(userInput == comInput){
-//         alert("비겼네요.다음기회에")
-//     }
-//     if(userInput == "보"){
-//         alert("당신이 이겼네요. ^^*")
-//     }
-//     if(userInput == "가위"){
-//         alert("컴퓨터가 이겼습니다 ^^*")
-//     }
-// }
-
+    if( userInput == "보"){
+        if(comInput == "보"){
+            result = "비김"
+        }
+        if(comInput == "가위"){
+            result ="컴이 이겼습니다"
+        }
+        if(comInput == "바위"){
+            result ="당신이 이겼습니다"
+        }
   
-    
-
-else{
-    
-    if(userInput == comInput){
-        alert("비겼네요.다음기회에")
     }
-    if(userInput == "가위"){
-        alert("당신이 이겼네요. ^^*")
-    }
-    if(userInput == "바위"){
-        alert("컴퓨터가 이겼습니다 ^^*")
-    }
+    console.log("내꺼 : " + userInput)
+    console.log("컴 : " + comInput)
+    console.log("결과 : " + result)
 }
-
-
 
 
 
---
+++ 20220628_for/js/script.js
@@ -0,0 +1,12 @@
+console.log('ok');
+let btn = document.querySelector("button.btn")[0]
+let box = document.querySelector("div.box")[0]
+
+document.getElementsByClassName("box")[0];
+btn.addEventListener("click",function(){
+    console.log('click');
+    box[0].innerHTML = "안녕1.";
+})
+let han =""
+let = num =0;
+let array = [1,2,3,4]
