116ac7ea8ad1547c30902918d2b0a0fdba5cc4ad
yycinm <yycinm@hanmail.net>
2022-08-03 17:14:58 +0900

    vvvv

--- 20220729_barmenu/js/script.js
+++ 20220729_barmenu/js/script.js
@@ -7,8 +7,26 @@
 // });
 
 
-$(".navbar ul").find("li").mouseover(over);
-$(".navbar ul").mouseout(out);
+// menu 0 1 2 3 4
+function nowMenu(menu){
+    console.log(menu)
+
+    if(menu == "main"){
+        $(".navbar ul").find("li").mouseover(over);
+        $(".navbar ul").mouseout(out);
+    }else{
+        $(".navbar li").eq(menu).addClass("active"); //현재 메뉴의 위치(after에서 점)
+        $(".bar").css({left:menu*240,opacity:1}); // 처음 바의 위치
+
+        $(".navbar ul").find("li").mouseover(over); // function over 호출
+        
+        $(".navbar ul").mouseout(function(){
+            $(".bar").css({left:menu*240,opacity:1}); // mouseout 시 원위치로 이동
+        });
+    }
+
+}
+
 
 function over(){
     let i = $(this).index();
@@ -17,4 +35,4 @@ function over(){
 }
 function out(){
     $(".bar").css({opacity:0})
-} 
+}
