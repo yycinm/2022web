64cdc5f42139d7320207f7dca0affeaff92fa18c
yycinm <yycinm@hanmail.net>
2022-10-21 18:11:39 +0900

    1021

---
+++ 00javascript/20220929_vue/data.js
@@ -0,0 +1,174 @@
+const noticeData = [
+    {
+        index:1,
+        cate:"공지사항",
+        title:"제14회 게임콘서트 개최 안내",
+        img:"01_01.jpg",
+        content:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, autem!",
+        url:"http://www.naver.com",
+        data:"2022.09.23"
+    },
+    {
+        index:2,
+        cate:"공지사항",
+        title:"넷마블게임박물관 건립준비캠페인",
+        img:"01_02.jpg",
+        content:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, autem!",
+        url:"http://www.naver.com",
+        data:"2022.09.14"
+    },
+    {
+        index:3,
+        cate:"공지사항",
+        title:"[게임소통학교] 도미노 영상 이벤트 결과 발표",
+        img:"01_03.jpg",
+        content:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, autem!",
+        url:"http://www.naver.com",
+        data:"2022.08.18"
+    },
+    {
+        index:4,
+        cate:"공지사항",
+        title:"제14회 게임콘서트 개최 안내",
+        img:"01_01.jpg",
+        content:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, autem!",
+        url:"http://www.naver.com",
+        data:"2022.09.23"
+    },
+    {
+        index:5,
+        cate:"공지사항",
+        title:"넷마블게임박물관 건립준비캠페인",
+        img:"01_02.jpg",
+        content:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, autem!",
+        url:"http://www.naver.com",
+        data:"2022.09.14"
+    },
+    {
+        index:6,
+        cate:"공지사항",
+        title:"[게임소통학교] 도미노 영상 이벤트 결과 발표",
+        img:"01_03.jpg",
+        content:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, autem!",
+        url:"http://www.naver.com",
+        data:"2022.08.18"
+    },
+   
+
+]
+const newsData = [
+    {
+        index:1,
+        cate:"뉴스룸",
+        title:"넷마블장애인선수단, ‘2022 추계전국장애인조정대회’에서 총 17개 메달 획득",
+        img:"02_01.jpg",
+        content:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, autem!",
+        url:"http://www.naver.com",
+        data:"2022.09.28"
+    },
+    {
+        index:2,
+        cate:"뉴스룸",
+        title:"게임아카데미 부트캠프 2기 온라인 수료식 개최",
+        img:"02_02.jpg",
+        content:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, autem!",
+        url:"http://www.naver.com",
+        data:"2022.09.26"
+    },
+    {
+        index:3,
+        cate:"뉴스룸",
+        title:"넷마블게임박물관 건립준비캠페인",
+        img:"02_03.jpg",
+        content:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, autem!",
+        url:"http://www.naver.com",
+        data:"2022.09.19"
+    },
+    {
+        index:4,
+        cate:"뉴스룸",
+        title:"넷마블장애인선수단, ‘2022 추계전국장애인조정대회’에서 총 17개 메달 획득",
+        img:"02_01.jpg",
+        content:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, autem!",
+        url:"http://www.naver.com",
+        data:"2022.09.28"
+    },
+    {
+        index:5,
+        cate:"뉴스룸",
+        title:"게임아카데미 부트캠프 2기 온라인 수료식 개최",
+        img:"02_02.jpg",
+        content:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, autem!",
+        url:"http://www.naver.com",
+        data:"2022.09.26"
+    },
+    {
+        index:6,
+        cate:"뉴스룸",
+        title:"넷마블게임박물관 건립준비캠페인",
+        img:"02_03.jpg",
+        content:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, autem!",
+        url:"http://www.naver.com",
+        data:"2022.09.19"
+    },
+   
+    
+]
+const snsData = [
+    {
+        index:1,
+        cate:"소셜미디어",
+        title:"<제12회 게임콘서트>바로보기",
+        img:"03_01.jpg",
+        content:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, autem!",
+        url:"http://www.naver.com",
+        data:"2022.08.10"
+    },
+    {
+        index:2,
+        cate:"소셜미디어",
+        title:"<제11회 게임콘서트 2부> Q&A",
+        img:"03_02.jpg",
+        content:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, autem!",
+        url:"http://www.naver.com",
+        data:"2022.08.10"
+    },
+    {
+        index:3,
+        cate:"소셜미디어",
+        title:"<제11회 게임콘서트 2부> 바로보기",
+        img:"03_03.jpg",
+        content:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, autem!",
+        url:"http://www.naver.com",
+        data:"2022.08.10"
+    },
+    {
+        index:4,
+        cate:"소셜미디어",
+        title:"<제12회 게임콘서트>바로보기",
+        img:"03_01.jpg",
+        content:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, autem!",
+        url:"http://www.naver.com",
+        data:"2022.08.10"
+    },
+    {
+        index:5,
+        cate:"소셜미디어",
+        title:"<제11회 게임콘서트 2부> Q&A",
+        img:"03_02.jpg",
+        content:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, autem!",
+        url:"http://www.naver.com",
+        data:"2022.08.10"
+    },
+    {
+        index:6,
+        cate:"소셜미디어",
+        title:"<제11회 게임콘서트 2부> 바로보기",
+        img:"03_03.jpg",
+        content:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illo, autem!",
+        url:"http://www.naver.com",
+        data:"2022.08.10"
+    },
+]
+
+export {noticeData,newsData,snsData};
